07a919df35757c388aaef79737b5dde3c96659fb
Mateo Martinez <mateo8mh@gmail.com>
2026-02-23 16:34:39 -0500

    Estructura Base terminada

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,3 +1,15 @@
-export const app = () => {
-    
+import html from './app.html?raw'; // El ?raw es para que se importe el contenido del archivo html como un string, en lugar de ser tratado como un módulo o recurso.
+
+/**
+ * 
+ * @param {String} elementId 
+ */
+export const App = (elementId) => {
+
+    // Cuando la funcion App sea ejecutada o se llama, se va a ejecutar esta función anónima
+    (() => {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
+    })();
 }
