329e86f6af2969a55b8a88c53c372c48231b5e12
Mateo Martinez <mateo8mh@gmail.com>
2026-02-24 16:59:07 -0500

    Metodos del store creados, falta hacer la implementación

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -21,6 +21,61 @@ const initStore = () => {
     console.log('Init Store');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+    
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId Todo identifier
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId
+ */
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * @returns {String} current filter
+ */
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
