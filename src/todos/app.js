426360670a4b7e8054cfede34db3867f7629e791
Mateo Martinez <mateo8mh@gmail.com>
2026-02-25 21:39:42 -0500

    Renderizando todos en el html

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,5 +1,10 @@
 import todoStore from '../store/todo.store';
 import html from './app.html?raw'; // El ?raw es para que se importe el contenido del archivo html como un string, en lugar de ser tratado como un módulo o recurso.
+import { renderTodos } from './uses-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
 
 /**
  * 
@@ -9,7 +14,7 @@ export const App = (elementId) => {
 
     const displayTodos = () => {
         const todos = todoStore.getTodos(todoStore.getCurrentFilter());
-        console.log(todos);
+        renderTodos(ElementIDs.TodoList, todos);
     }
 
     // Cuando la funcion App sea ejecutada o se llama, se va a ejecutar esta función anónima
---
+++ src/todos/uses-cases/render-todos.js
@@ -0,0 +1,11 @@
+import { Todo } from "../models/todo-model";
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = (elementId, todos = []) => {
+    //TODO: referencia
+    const element = document.querySelector(elementId);
+}
