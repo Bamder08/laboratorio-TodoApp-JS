03cb82bb5feb8961a61b8f95f03bfbdcb79990a8
Mateo Martinez <mateo8mh@gmail.com>
2026-02-25 22:30:23 -0500

    Renderizado HTML para todos los TODOS terminado

--- src/todos/uses-cases/render-todos.js
+++ src/todos/uses-cases/render-todos.js
@@ -1,14 +1,18 @@
 import { Todo } from "../models/todo-model";
 import { createTodoHTML } from "./create-todo-html";
 
+let element;
+
 /**
  * 
  * @param {String} elementId 
  * @param {Todo} todos 
  */
 export const renderTodos = (elementId, todos = []) => {
-    //TODO: referencia
-    const element = document.querySelector(elementId);
+
+    if(!element) element = document.querySelector(elementId);
+    if(!element) throw new Error(`Element${elementId} not found in the DOM`);
+    element.innerHTML = '';
 
     todos.forEach(todo => {
         element.append(createTodoHTML(todo));
