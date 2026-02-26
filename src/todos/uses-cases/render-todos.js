392bdfdcaba3143a781547633d4c019fca5006aa
Mateo Martinez <mateo8mh@gmail.com>
2026-02-25 21:42:39 -0500

    Renderizando todos en el html

--- src/todos/uses-cases/render-todos.js
+++ src/todos/uses-cases/render-todos.js
@@ -8,4 +8,8 @@ import { Todo } from "../models/todo-model";
 export const renderTodos = (elementId, todos = []) => {
     //TODO: referencia
     const element = document.querySelector(elementId);
+
+    todos.forEach(todo => {
+        
+    });
 }
